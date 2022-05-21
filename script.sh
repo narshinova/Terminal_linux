3c6f329008320d1349520dcb3e3440efbc1758a5
narshinova <Nataliaarshinova@mail.ru>
2022-05-21 15:01:56 +0300

    update file script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ cd foldername
 mkdir foldername1 foldername2  foldername3
 cd foldername1
 touch 1.txt 2.txt 3.txt 4.json 5.json
-mkdir foldername4  foldername5  foldername6
+mkdir foldername4  foldername5  foldername7
 ls -la
 mv 1.txt 2.txt foldername4/ 
