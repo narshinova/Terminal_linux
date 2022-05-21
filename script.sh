5b79b4860b4f6e51c4c6624bc14421eaaf1c8086
narshinova <Nataliaarshinova@mail.ru>
2022-05-21 14:55:35 +0300

    add file readme.txt

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+cd foldername
+mkdir foldername1 foldername2  foldername3
+cd foldername1
+touch 1.txt 2.txt 3.txt 4.json 5.json
+mkdir foldername4  foldername5  foldername6
+ls -la
+mv 1.txt 2.txt foldername4/ 
